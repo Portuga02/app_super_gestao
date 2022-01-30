03dda34fe5fdb3f7a001ad47dcfaae41779a1c0d
Sávio Gomes da Silva <saviogomesdasilva@outlook.com.br>
2022-01-30 12:09:49 -0300

    Configurando titulos para paginas

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -8,6 +8,6 @@ class ContatosController extends Controller
 {
     public function contatos()
     {
-        return view('site.contato');
+        return view('site.contato', ['tituloContato' => 'Contato']);
     }
 }
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -1,4 +1,5 @@
 @extends('site.layouts.basico')
+@section('titulo', $LoginTitle)
 @section('conteudo')
     <h1>Tela de login</h1>
 @endsection
