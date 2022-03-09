ed5e791a4ff5bba146be4a4130a8493b268a371a
Sávio Gomes da Silva <saviogomesdasilva@outlook.com.br>
2022-03-08 21:56:09 -0300

    criando migrates dentre outros

---
+++ database/migrations/2022_03_08_234534_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn('uf');  // se pode remover as colunas de forma individual
+            $table->dropColumn(['uf', 'email']) ; // e também pode remover colunas de forma por array se precisar eliminar mais de uma coluna
+        });
+    }
+}
