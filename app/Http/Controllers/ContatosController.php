d8856b5984c721e03c730fb4902c09ab0a9fa58a
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-06-29 21:56:39 -0300

    ajustes te criação de crud simples para cadastrar no banco

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
+use Exception;
 
 class ContatosController extends Controller
 {
     public function contatos(Request $request)
     {
-
-        return view('site.contato', ['tituloContato' => 'Contato']);
-
+        try {
+            $contato = new SiteContato();
+            $contato->create($request->all());
+  return view('site.contato', ['tituloContato' => 'Contato']);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'info' => 'error',
+                'result' => 'It was not possible to get data.',
+                'error' => $e->getMessage() . $e->getLine(),
+            ], 400);
+        }
     }
 }
