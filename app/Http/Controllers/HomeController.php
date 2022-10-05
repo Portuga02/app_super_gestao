c557909c7dc5d4a7b13e80c033d2e056c3485c0f
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-10-04 21:10:50 -0300

    ajuste de rota

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,11 @@ class HomeController extends Controller
 {
     public function Home()
     {
-        return view('site.home', ['tituloHome' => 'Home']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view('site.home', ['tituloHome' => 'Home', 'motivo_contatos' => $motivo_contatos]);
     }
 }
-
