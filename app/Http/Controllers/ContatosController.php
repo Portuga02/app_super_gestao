32deac0df9afdf8b9afb313f38319ad741165f01
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-11-15 17:04:10 -0300

    ajustando arquivos

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -29,14 +29,29 @@ class ContatosController extends Controller
     {
 
         if (!empty($request)) {
-            $request->validate([
-                'nome' => 'required|min:3|max:40',
-                'telefone' => 'required',
-                'email' => 'email|unique:site_contatos',
-                'motivo_contatos_id' => 'required',
-                'mensagem' => 'required|max:2000'
+            $request->validate(
+                $regras =  [
+                    'nome' => 'required|min:3|max:40',
+                    'telefone' => 'required',
+                    'email' => 'required|email|unique:site_contatos',
+                    'motivo_contatos_id' => 'required',
+                    'mensagem' => 'required|max:2000'
 
-            ]);
+                ],
+                $feedback =  [
+                    'nome.required' => 'O campo nome precisa ser preenchido.',
+                    'nome.min' => 'O campo nome precisa ter no minimo três caracteres',
+                    'nome.max' => 'o campo nome tem que ter no maximo quarenta caracteres',
+                    'telefone.required' => 'É necessário um endereço de email valido para retorno',
+                    'email.required' => 'o campo email precisa ser preenchido',
+                    'motivo_contatos_id.required' => 'É necessário um motivo para o contato ',
+                    'mensagem.required' => 'É necessario uma mensagem para o contato conosco',
+                    'mensagem.max' => 'deve ter no maximo 2000 caracteres'
+                ],
+                // para valores default 
+                ['required' => 'campo :atribute deve ser preechido']
+            );
+            $request->validate($regras, $feedback);
             SiteContato::create($request->all());
             return redirect()->route('site.Home');
         } else {
