c59d4f465562b22bdf4a4804c3ba36a30cbe5bac
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-11-14 21:09:20 -0300

    criando novas regras de funções e ajustes try-catch e ajuste de validações de campos

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -6,32 +6,47 @@ use App\Models\MotivoContato;
 use Illuminate\Http\Request;
 use App\Models\SiteContato;
 
+use Symfony\Component\HttpFoundation\Response;
 
 class ContatosController extends Controller
 {
     public function contatos(Request $request)
     {
         try {
-
             $motivo_contatos = MotivoContato::all();
             return view('site.contato', ['tituloContato' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
-        } catch (\Throwable $th) {
-            throw $th;
+        } catch (\Exception $e) {
+            return response()->json([
+                'info' => 'error',
+
+                'error' => $e->getMessage(),
+                'Linha' => $e->getLine(),
+                'Arquivo' => $e->getFile()
+            ], Response::HTTP_BAD_REQUEST);
         }
     }
     public function salvar(Request $request)
     {
+        try {
+            if (!empty($request)) {
+                $request->validate([
+                    'nome' => 'required|min:3|max:40',
+                    'telefone' => 'required',
+                    'email' => 'email',
+                    'motivo_contatos_id' => 'required',
+                    'mensagem' => 'required|max:2000'
 
-        if (!empty($request)) {
-            $request->validate([
-                'nome' => 'required|min:3|max:40',
-                'telefone' => 'required',
-                'email' => 'email',
-                'motivo_contato' => 'required'
-                
-            ]);
-            SiteContato::create($request->all());
-            return redirect()->route('');
+                ]);
+                SiteContato::create($request->all());
+                return redirect()->route('site.Home');
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'info' => 'error',
+                'error' => $e->getMessage(),
+                'Linha' => $e->getLine(),
+                'Arquivo' => $e->getFile()
+            ], Response::HTTP_BAD_REQUEST);
         }
     }
 }
